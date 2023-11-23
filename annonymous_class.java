d03b0a3c0af41fc253d123af04082d69427b2279
Parth Takale <130144456+Parthtakale06@users.noreply.github.com>
2023-11-23 23:44:37 +0530

    Add files via upload

---
+++ annonymous_class.java
@@ -0,0 +1,37 @@
+package Static;
+ interface a{
+void show();
+
+ }
+
+public class annonymous {
+    public static void main(String[] args) {
+
+a obj = new a(){
+
+   public  void show(){
+        System.out.println("java8");
+    }
+};
+
+obj.show();
+       
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
